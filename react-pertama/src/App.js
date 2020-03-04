4c57ad2fa8d0a058bbc099e14aee1a97296329f8
rstx <rstx32@gmail.com>
2020-03-04 20:58:03 +0700

    meh

--- react-pertama/src/App.js
+++ react-pertama/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          rstx
+          rstx, it's me!
         </p>
         <a
           className="App-link"
