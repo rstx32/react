9f6fdfbd349641d6db9c62628ff3e825e3e41763
rstx <rstx32@gmail.com>
2020-03-04 21:16:32 +0700

    create new function

--- react-pertama/src/index.js
+++ react-pertama/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+  
+function Halo(){
+    return(
+      <div>
+        <h1>belajar fungsi reactjs</h1>
+        <h2>belajar fungsi reactjs</h2>
+      </div>
+    );
+  }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Halo />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
